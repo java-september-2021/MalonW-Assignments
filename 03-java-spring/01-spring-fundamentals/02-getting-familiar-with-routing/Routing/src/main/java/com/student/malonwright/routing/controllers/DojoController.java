f196db8286bf6d19ac0641a9ef97a5f56b08aed8
Malon Wright <malonw@pacbell.net>
2021-09-14 18:27:44 -0700

    Finished Routing

--- 03-java-spring/01-spring-fundamentals/02-getting-familiar-with-routing/Routing/src/main/java/com/student/malonwright/routing/controllers/DojoController.java
+++ 03-java-spring/01-spring-fundamentals/02-getting-familiar-with-routing/Routing/src/main/java/com/student/malonwright/routing/controllers/DojoController.java
@@ -4,19 +4,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
-@RequestMapping("dojo/{string}")
-@ResponseBody
+@RequestMapping("/dojo")
+
 public class DojoController {
-	@RequestMapping("/dojo")
-	public String dojo( ) {
-		return "The dojo is awesome!";
+	@RequestMapping("/{string}")
+	public String dojo( @PathVariable("string") String string) {
+		if (string.equals("dojo")) {
+			return "The dojo is awesome!";
+		}else if (string.equals("burbank")) {
+			return "Burbank Dojo is located in Southern California";
+	}else if(string.equals("sanjose")){
+			return "SJ dojo is the headquarters";
+	}else {
+		return "Location not found";
+	
 	}
-@ResponseBody
-	public String burbank(@PathVariable String string) {
-		return "Burbank Dojo is located in Southern California";
-	}
-@ResponseBody
-	public String sanjose(@PathVariable String string) {
-		return "SJ dojo is the headquarters";
-	}
+}
 }
