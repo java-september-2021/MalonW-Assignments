e73ea924e213a62aeb2b81943f6891b969154610
Malon Wright <83512002+malonw@users.noreply.github.com>
2021-09-20 16:30:19 -0700

    still working on Survey

--- 03-java-spring/01-spring-fundamentals/07-dojo-survey/DojoiSurvey/src/main/java/com/malonwright/DojoSurvey/controller/FormController.java
+++ 03-java-spring/01-spring-fundamentals/07-dojo-survey/DojoiSurvey/src/main/java/com/malonwright/DojoSurvey/controller/FormController.java
@@ -1,28 +1,34 @@
 package com.malonwright.DojoSurvey.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+//Should Display Form
 @Controller
 public class FormController {
-	@RequestMapping(value="/", method =RequestMethod.GET)
+	@GetMapping(value="/")
 	public String index() {
 		return "index.jsp";
 	}
-	@RequestMapping(value="/result", method=RequestMethod.POST)
-	public String results(@RequestParam(value="name")String name,@RequestParam(value="dojo") String dojo,@RequestParam(value="language")String language,@RequestParam(value="comment") String comment, ModelMap modelmap){
-		modelmap.put("name", name);
-		modelmap.put("dojo", dojo);
-		modelmap.put("language", language);
-		modelmap.put("comment", comment);
+	
+	//Takes data from form submission assigns to key, value pairs
+	@PostMapping(value="/result")
+	public String results(@RequestParam(value="name")String name,@RequestParam(value="dojo") String dojo,@RequestParam(value="language")String language,@RequestParam(value="comment") String comment, Model model){
+
+		
+		model.addAttribute("name", name);
+		model.addAttribute("dojo", dojo);
+		model.addAttribute("language", language);
+		model.addAttribute("comment", comment);
 		
 		
 		return "redirect:/results";
 	}
-	@RequestMapping(value="/results", method=RequestMethod.GET)
+	//Displays results page with data taken in from form submission.
+	@GetMapping(value="/results")
 	public String results() {
 		
 		return "results.jsp";
