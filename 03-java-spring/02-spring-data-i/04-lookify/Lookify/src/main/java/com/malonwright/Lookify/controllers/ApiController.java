0903f1c81a15f3b486f3106040b5f38a52e60892
Malon Wright <83512002+malonw@users.noreply.github.com>
2021-10-01 14:34:09 -0700

    Working on Lookify

---
+++ 03-java-spring/02-spring-data-i/04-lookify/Lookify/src/main/java/com/malonwright/Lookify/controllers/ApiController.java
@@ -0,0 +1,30 @@
+package com.malonwright.Lookify.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.malonwright.Lookify.models.Song;
+import com.malonwright.Lookify.services.songService;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+
+	@Autowired
+	private songService sService;
+	
+	@GetMapping("/songList")
+	public List<Song> songsList(){
+		return this.sService.getAllSongs();
+		
+	}
+	@PostMapping("/create")
+	public Song createSong(Song song) {
+		return this.sService.createSong(song);
+	}
+}
